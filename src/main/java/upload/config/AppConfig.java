ecd53df2ab6bb8a4961217b2fe1327ed8e300024
Kira <datdhvinh@gmail.com>
2020-07-08 21:36:34 +0700

    upload file finish

--- src/main/java/upload/config/AppConfig.java
+++ src/main/java/upload/config/AppConfig.java
@@ -1,18 +1,24 @@
 package upload.config;
 
+import org.apache.commons.fileupload.FileItem;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-//@PropertySource("uploadfile.properties")
+@PropertySource("classpath:uploadfile.properties")
 @ComponentScan("upload.controller")
 public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
     ApplicationContext applicationContext;
@@ -30,4 +36,20 @@ public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationCon
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(20971520);
+        return multipartResolver;
+    }
+//
+//    @Autowired
+//    Environment environment;
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry){
+//        String fileUpload = environment.getProperty("file_upload").toString();
+//        registry.addResourceHandler("/i/**").addResourceLocations("file"+fileUpload);
+//    }
 }
